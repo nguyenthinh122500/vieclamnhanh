6a0865d7b4aaab9199b74708c85e08f41dea6dff
nguyenthinh122500 <nguyenphuthinh102500@gmail.com>
2023-11-29 09:53:19 +0700

    upcode

--- src/pages/JobDetail/BodyDetail/Related/Related.js
+++ src/pages/JobDetail/BodyDetail/Related/Related.js
@@ -1,221 +1,213 @@
-import React from 'react'
+import React from "react";
 
 export default function Related() {
   return (
     <div className="row">
-    <div className="col-lg-12">
-      <h4 className="mb-4">Related Jobs :</h4>
-      <div className="recent-jobs owl-carousel owl-theme">
-        <div className="card card-style10">
-          <div className="card-body">
-            <span className="popular-jobs-status">Full Time</span>
-            <a className="favourite" href="job-details-2.html#">
-              <i className="far fa-heart" />
-            </a>
-            <div className="popular-jobs-box">
-              <img
-                className="mb-4 border-radius-10"
-                src="img/content/job-02.jpg"
-                alt="..."
-              />
-              <h4 className="h5">
-                <a href="job-details.html">Creative Tech Ltd.</a>
-              </h4>
-              <p className="text-muted font-weight-500">
-                Sr. Project Manager
-              </p>
+      <div className="col-lg-12">
+        <h4 className="mb-4">Related Jobs :</h4>
+        <div className="recent-jobs owl-carousel owl-theme">
+          <div className="card card-style10">
+            <div className="card-body">
+              <span className="popular-jobs-status">Full Time</span>
+              <a className="favourite" href="job-details-2.html#">
+                <i className="far fa-heart" />
+              </a>
+              <div className="popular-jobs-box">
+                <img
+                  className="mb-4 border-radius-10"
+                  src="img/content/job-02.jpg"
+                  alt="..."
+                />
+                <h4 className="h5">
+                  <a href="job-details.html">Creative Tech Ltd.</a>
+                </h4>
+                <p className="text-muted font-weight-500">
+                  Sr. Project Manager
+                </p>
+              </div>
+              <span className="border-end border-color-extra-light-gray pe-2 me-2">
+                <i className="fas fa-map-marker-alt pe-2 text-secondary" />
+                California
+              </span>
+              <span className="border-end border-color-extra-light-gray pe-2 me-2">
+                <i className="far fa-clock pe-2 text-secondary" />
+                $15K
+              </span>
+              <span>
+                <i className="ti-briefcase pe-2 text-secondary" />3 year
+              </span>
             </div>
-            <span className="border-end border-color-extra-light-gray pe-2 me-2">
-              <i className="fas fa-map-marker-alt pe-2 text-secondary" />
-              California
-            </span>
-            <span className="border-end border-color-extra-light-gray pe-2 me-2">
-              <i className="far fa-clock pe-2 text-secondary" />
-              $15K
-            </span>
-            <span>
-              <i className="ti-briefcase pe-2 text-secondary" />3 year
-            </span>
-          </div>
-          <a href="job-details.html" className="butn butn-apply">
-            Apply Now
-          </a>
-        </div>
-        <div className="card card-style10">
-          <div className="card-body">
-            <span className="popular-jobs-status">Part Time</span>
-            <a className="favourite" href="job-details-2.html#">
-              <i className="far fa-heart" />
+            <a href="job-details.html" className="butn butn-apply">
+              Apply Now
             </a>
-            <div className="popular-jobs-box">
-              <img
-                className="mb-4 border-radius-10"
-                src="img/content/job-03.jpg"
-                alt="..."
-              />
-              <h4 className="h5">
-                <a href="job-details.html">Bedford Ltd.</a>
-              </h4>
-              <p className="text-muted font-weight-500">
-                UI / UX Designer
-              </p>
-            </div>
-            <span className="border-end border-color-extra-light-gray pe-2 me-2">
-              <i className="fas fa-map-marker-alt pe-2 text-secondary" />
-              New York
-            </span>
-            <span className="border-end border-color-extra-light-gray pe-2 me-2">
-              <i className="far fa-clock pe-2 text-secondary" />
-              $18K
-            </span>
-            <span>
-              <i className="ti-briefcase pe-2 text-secondary" />8 year
-            </span>
           </div>
-          <a href="job-details.html" className="butn butn-apply">
-            Apply Now
-          </a>
-        </div>
-        <div className="card card-style10">
-          <div className="card-body">
-            <span className="popular-jobs-status">Full Time</span>
-            <a className="favourite" href="job-details-2.html#">
-              <i className="far fa-heart" />
+          <div className="card card-style10">
+            <div className="card-body">
+              <span className="popular-jobs-status">Part Time</span>
+              <a className="favourite" href="job-details-2.html#">
+                <i className="far fa-heart" />
+              </a>
+              <div className="popular-jobs-box">
+                <img
+                  className="mb-4 border-radius-10"
+                  src="img/content/job-03.jpg"
+                  alt="..."
+                />
+                <h4 className="h5">
+                  <a href="job-details.html">Bedford Ltd.</a>
+                </h4>
+                <p className="text-muted font-weight-500">UI / UX Designer</p>
+              </div>
+              <span className="border-end border-color-extra-light-gray pe-2 me-2">
+                <i className="fas fa-map-marker-alt pe-2 text-secondary" />
+                New York
+              </span>
+              <span className="border-end border-color-extra-light-gray pe-2 me-2">
+                <i className="far fa-clock pe-2 text-secondary" />
+                $18K
+              </span>
+              <span>
+                <i className="ti-briefcase pe-2 text-secondary" />8 year
+              </span>
+            </div>
+            <a href="job-details.html" className="butn butn-apply">
+              Apply Now
             </a>
-            <div className="popular-jobs-box">
-              <img
-                className="mb-4 border-radius-10"
-                src="img/content/job-04.jpg"
-                alt="..."
-              />
-              <h4 className="h5">
-                <a href="job-details.html">Saspol Limited</a>
-              </h4>
-              <p className="text-muted font-weight-500">
-                Software Engineer
-              </p>
-            </div>
-            <span className="border-end border-color-extra-light-gray pe-2 me-2">
-              <i className="fas fa-map-marker-alt pe-2 text-secondary" />
-              London
-            </span>
-            <span className="border-end border-color-extra-light-gray pe-2 me-2">
-              <i className="far fa-clock pe-2 text-secondary" />
-              $20K
-            </span>
-            <span>
-              <i className="ti-briefcase pe-2 text-secondary" />2 year
-            </span>
           </div>
-          <a href="job-details.html" className="butn butn-apply">
-            Apply Now
-          </a>
-        </div>
-        <div className="card card-style10">
-          <div className="card-body">
-            <span className="popular-jobs-status">Part Time</span>
-            <a className="favourite" href="job-details-2.html#">
-              <i className="far fa-heart" />
+          <div className="card card-style10">
+            <div className="card-body">
+              <span className="popular-jobs-status">Full Time</span>
+              <a className="favourite" href="job-details-2.html#">
+                <i className="far fa-heart" />
+              </a>
+              <div className="popular-jobs-box">
+                <img
+                  className="mb-4 border-radius-10"
+                  src="img/content/job-04.jpg"
+                  alt="..."
+                />
+                <h4 className="h5">
+                  <a href="job-details.html">Saspol Limited</a>
+                </h4>
+                <p className="text-muted font-weight-500">Software Engineer</p>
+              </div>
+              <span className="border-end border-color-extra-light-gray pe-2 me-2">
+                <i className="fas fa-map-marker-alt pe-2 text-secondary" />
+                London
+              </span>
+              <span className="border-end border-color-extra-light-gray pe-2 me-2">
+                <i className="far fa-clock pe-2 text-secondary" />
+                $20K
+              </span>
+              <span>
+                <i className="ti-briefcase pe-2 text-secondary" />2 year
+              </span>
+            </div>
+            <a href="job-details.html" className="butn butn-apply">
+              Apply Now
             </a>
-            <div className="popular-jobs-box">
-              <img
-                className="mb-4 border-radius-10"
-                src="img/content/job-05.jpg"
-                alt="..."
-              />
-              <h4 className="h5">
-                <a href="job-details.html">Depan insider ltd</a>
-              </h4>
-              <p className="text-muted font-weight-500">
-                Content Writing
-              </p>
-            </div>
-            <span className="border-end border-color-extra-light-gray pe-2 me-2">
-              <i className="fas fa-map-marker-alt pe-2 text-secondary" />
-              UK
-            </span>
-            <span className="border-end border-color-extra-light-gray pe-2 me-2">
-              <i className="far fa-clock pe-2 text-secondary" />
-              $40K
-            </span>
-            <span>
-              <i className="ti-briefcase pe-2 text-secondary" />7 year
-            </span>
           </div>
-          <a href="job-details.html" className="butn butn-apply">
-            Apply Now
-          </a>
-        </div>
-        <div className="card card-style10">
-          <div className="card-body">
-            <span className="popular-jobs-status">Full Time</span>
-            <a className="favourite" href="job-details-2.html#">
-              <i className="far fa-heart" />
+          <div className="card card-style10">
+            <div className="card-body">
+              <span className="popular-jobs-status">Part Time</span>
+              <a className="favourite" href="job-details-2.html#">
+                <i className="far fa-heart" />
+              </a>
+              <div className="popular-jobs-box">
+                <img
+                  className="mb-4 border-radius-10"
+                  src="img/content/job-05.jpg"
+                  alt="..."
+                />
+                <h4 className="h5">
+                  <a href="job-details.html">Depan insider ltd</a>
+                </h4>
+                <p className="text-muted font-weight-500">Content Writing</p>
+              </div>
+              <span className="border-end border-color-extra-light-gray pe-2 me-2">
+                <i className="fas fa-map-marker-alt pe-2 text-secondary" />
+                UK
+              </span>
+              <span className="border-end border-color-extra-light-gray pe-2 me-2">
+                <i className="far fa-clock pe-2 text-secondary" />
+                $40K
+              </span>
+              <span>
+                <i className="ti-briefcase pe-2 text-secondary" />7 year
+              </span>
+            </div>
+            <a href="job-details.html" className="butn butn-apply">
+              Apply Now
             </a>
-            <div className="popular-jobs-box">
-              <img
-                className="mb-4 border-radius-10"
-                src="img/content/job-06.jpg"
-                alt="..."
-              />
-              <h4 className="h5">
-                <a href="job-details.html">Oryx International</a>
-              </h4>
-              <p className="text-muted font-weight-500">.Net Developer</p>
-            </div>
-            <span className="border-end border-color-extra-light-gray pe-2 me-2">
-              <i className="fas fa-map-marker-alt pe-2 text-secondary" />
-              Japan
-            </span>
-            <span className="border-end border-color-extra-light-gray pe-2 me-2">
-              <i className="far fa-clock pe-2 text-secondary" />
-              $90K
-            </span>
-            <span>
-              <i className="ti-briefcase pe-2 text-secondary" />1 year
-            </span>
           </div>
-          <a href="job-details.html" className="butn butn-apply">
-            Apply Now
-          </a>
-        </div>
-        <div className="card card-style10">
-          <div className="card-body">
-            <span className="popular-jobs-status">Full Time</span>
-            <a className="favourite" href="job-details-2.html#!">
-              <i className="far fa-heart" />
+          <div className="card card-style10">
+            <div className="card-body">
+              <span className="popular-jobs-status">Full Time</span>
+              <a className="favourite" href="job-details-2.html#">
+                <i className="far fa-heart" />
+              </a>
+              <div className="popular-jobs-box">
+                <img
+                  className="mb-4 border-radius-10"
+                  src="img/content/job-06.jpg"
+                  alt="..."
+                />
+                <h4 className="h5">
+                  <a href="job-details.html">Oryx International</a>
+                </h4>
+                <p className="text-muted font-weight-500">.Net Developer</p>
+              </div>
+              <span className="border-end border-color-extra-light-gray pe-2 me-2">
+                <i className="fas fa-map-marker-alt pe-2 text-secondary" />
+                Japan
+              </span>
+              <span className="border-end border-color-extra-light-gray pe-2 me-2">
+                <i className="far fa-clock pe-2 text-secondary" />
+                $90K
+              </span>
+              <span>
+                <i className="ti-briefcase pe-2 text-secondary" />1 year
+              </span>
+            </div>
+            <a href="job-details.html" className="butn butn-apply">
+              Apply Now
             </a>
-            <div className="popular-jobs-box">
-              <img
-                className="mb-4 border-radius-10"
-                src="img/content/job-07.jpg"
-                alt="..."
-              />
-              <h4 className="h5">
-                <a href="job-details.html">Mingo Tech</a>
-              </h4>
-              <p className="text-muted font-weight-500">
-                Digital Marketing
-              </p>
-            </div>
-            <span className="border-end border-color-extra-light-gray pe-2 me-2">
-              <i className="fas fa-map-marker-alt pe-2 text-secondary" />
-              Afghanistan
-            </span>
-            <span className="border-end border-color-extra-light-gray pe-2 me-2">
-              <i className="far fa-clock pe-2 text-secondary" />
-              $35K
-            </span>
-            <span>
-              <i className="ti-briefcase pe-2 text-secondary" />2 year
-            </span>
           </div>
-          <a href="job-details.html" className="butn butn-apply">
-            Apply Now
-          </a>
+          <div className="card card-style10">
+            <div className="card-body">
+              <span className="popular-jobs-status">Full Time</span>
+              <a className="favourite" href="job-details-2.html#!">
+                <i className="far fa-heart" />
+              </a>
+              <div className="popular-jobs-box">
+                <img
+                  className="mb-4 border-radius-10"
+                  src="img/content/job-07.jpg"
+                  alt="..."
+                />
+                <h4 className="h5">
+                  <a href="job-details.html">Mingo Tech</a>
+                </h4>
+                <p className="text-muted font-weight-500">Digital Marketing</p>
+              </div>
+              <span className="border-end border-color-extra-light-gray pe-2 me-2">
+                <i className="fas fa-map-marker-alt pe-2 text-secondary" />
+                Afghanistan
+              </span>
+              <span className="border-end border-color-extra-light-gray pe-2 me-2">
+                <i className="far fa-clock pe-2 text-secondary" />
+                $35K
+              </span>
+              <span>
+                <i className="ti-briefcase pe-2 text-secondary" />2 year
+              </span>
+            </div>
+            <a href="job-details.html" className="butn butn-apply">
+              Apply Now
+            </a>
+          </div>
         </div>
       </div>
     </div>
-  </div>
-  )
+  );
 }
